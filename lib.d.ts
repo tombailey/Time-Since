4c8ace776f24d34122fd23b5b03efb5f2824089b
Trevor Coleman <trevor@getdunbar.com>
2019-04-11 00:04:00 -0400

    Change types to anonymous functions that return number

--- lib.d.ts
+++ lib.d.ts
@@ -1,12 +1,10 @@
 declare module "time-since" {
-  export interface timeSince {
-    since: {
-      millis: number;
-      secs: number;
-      mins: number;
-      hours: number;
-      days: number;
-      weeks: number;
-    };
-  }
+  export function since (timestamp: Date): {
+    "millis": ()=>number,
+    "secs": ()=>number,
+    "mins": ()=>number,
+    "hours": ()=>number,
+    "days": ()=>number,
+    "weeks": ()=>number,
+}
 }
